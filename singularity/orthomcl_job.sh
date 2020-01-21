5b479286bb77a94210f439befec75da52731c184
Jordan Hayes <hayesjordan@gmail.com>
2020-01-21 14:45:49 -0800

    Create orthomcl_job.sh

---
+++ singularity/orthomcl_job.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+##SBATCH -o my_output_%j
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=useremail@address.com
+#SBATCH --time=70:00:00
+#SBATCH --job-name=orthomcl
+#SBATCH -p intel
+
+module load singularity
+module load orthomcl
+
+# Go to database directory
+cd ~/bigdata/mysql
+
+# Start Database
+PORT=$(singularity exec --writable-tmpfs -B db/:/var/lib/mysql mariadb.sif grep -oP '^port = \K\d{4}' /etc/mysql/my.cnf | head -1)
+singularity instance start --writable-tmpfs -B db/:/var/lib/mysql mariadb.sif mysqldb
+sleep 10
+
+# Move to bigdata
+cd ~/bigdata/
+
+# Update Orthomcl.config
+sed -i "s/^dbConnectString.*$/dbConnectString=dbi:mysql:orthomcl:${HOSTNAME}:${PORT}/" orthomcl/orthomcl.config
+
+# Run orthomcl
+orthomclInstallSchema orthomcl/orthomcl.config orthomcl/install_schema.log
+
+# Stop Database
+singularity instance stop mysqldb
