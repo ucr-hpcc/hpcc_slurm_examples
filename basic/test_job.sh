4d35013835a8c9f0f523dd952d513c4abab3ff2a
Jordan Hayes <1438266+jdhayes@users.noreply.github.com>
2020-04-08 11:48:27 -0700

    Update test_job.sh

--- basic/test_job.sh
+++ basic/test_job.sh
@@ -5,7 +5,7 @@
 #SBATCH -p short,batch
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=0-00:15:00     # 15 minutes
-##SBATCH --mail-user=jhayes@ucr.edu
+##SBATCH --mail-user=email@address.com
 ##SBATCH --mail-type=ALL
 ##SBATCH --job-name="just_a_test"
 
