ee35fe1d0dee29e585c342e5928796f70b0fb3f7
Jordan Hayes <jhayes@ucr.edu>
2020-12-18 11:36:57 -0800

    Create statsdept_test_job.sh

---
+++ basic/statsdept_test_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH -p statsdept
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=0-00:15:00     # 15 minutes
+##SBATCH --mail-user=email@address.com
+##SBATCH --mail-type=ALL
+##SBATCH --job-name="just_a_test"
+
+date
+sleep 60
+hostname
