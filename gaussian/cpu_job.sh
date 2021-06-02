715da65e70b292f6d5577d33cf3d091a99abdd81
Jordan Hayes <jhayes@ucr.edu>
2021-06-02 15:17:12 -0700

    Use workspace module

--- gaussian/cpu_job.sh
+++ gaussian/cpu_job.sh
@@ -9,14 +9,11 @@
 module load gaussian/16
 
 # Set scratch directory
-export GAUSS_SCRDIR=/scratch/${USER}/${SLURM_JOB_ID}
-mkdir -p ${GAUSS_SCRDIR}
+module load workspace/scratch
+export GAUSS_SCRDIR=${SCRATCH}
 
 # Move to working directory 
 cd ~/bigdata/Projects/gaussian/
 
 # Run Gaussian
 g16 ch4_opt.gjf
-
-# Delete old temp files
-rm -rf ${GAUSS_SCRDIR}
