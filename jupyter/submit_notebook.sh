f161aca63d41dce8e212951df4728cddb011c6da
Jordan Hayes <jhayes@ucr.edu>
2022-05-12 15:28:59 -0700

    Update submit_notebook.sh

--- jupyter/submit_notebook.sh
+++ jupyter/submit_notebook.sh
@@ -8,7 +8,7 @@
 #SBATCH --output=jupyter-notebook-%J.log
 
 # Load and activate jupyter conda environment
-module avail jupyterlab
+module load jupyterlab
 
 # Execute the notebook and generate HTML (notebook.html) as output file
 jupyter nbconvert --to html --execute notebook.ipynb
